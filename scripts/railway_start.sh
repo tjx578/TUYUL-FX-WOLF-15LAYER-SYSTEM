70f1c7aaa7e1f0906c547c7e823f0644451230fb
kadek tajox <Kadektjx@gmail.com>
2026-02-27 19:16:47 +0700

    Route gunicorn startup logs to stdout on Railway

--- scripts/railway_start.sh
+++ scripts/railway_start.sh
@@ -17,6 +17,6 @@ exec gunicorn api_server:app \
   --workers "${WEB_CONCURRENCY}" \
   --worker-class uvicorn.workers.UvicornWorker \
   --timeout 120 \
-  --access-logfile - \
-  --error-logfile - \
+  --access-logfile /dev/stdout \
+  --error-logfile /dev/stdout \
   --log-level info
