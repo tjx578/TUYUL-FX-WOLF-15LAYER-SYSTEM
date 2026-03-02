eb678a85860cd0106e8b8102bf32a6c345fe6fb9
kadek tajox <Kadektjx@gmail.com>
2026-03-03 02:47:47 +0800

    Update __init__.py

--- propfirm_manager/profiles/__init__.py
+++ propfirm_manager/profiles/__init__.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 """
 Prop Firm Profiles Package
 
 Contains guard implementations for different prop firms.
 """
+=======
+"""Propfirm profiles package."""
+>>>>>>> Stashed changes
